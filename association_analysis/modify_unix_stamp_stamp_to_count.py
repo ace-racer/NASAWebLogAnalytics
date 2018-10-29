65e6155414fd28188f4880777ced8a9ffe5bb19a
Pier <madstrum@gmail.com>
2018-10-30 02:43:42 +0800

    some sequence seen

--- association_analysis/modify_unix_stamp_stamp_to_count.py
+++ association_analysis/modify_unix_stamp_stamp_to_count.py
@@ -2,12 +2,14 @@ import pandas as pd
 import os
 
 os.chdir('C:\\Users\\pierl\\PycharmProjects\\NASA\\association_analysis')
-df = pd.read_csv('nasa_data\\sessionized_data\\sessionize_hostonly_july_ordered.csv')
+df = pd.read_csv('nasa_data\\sessionized_data\\sessionize_hostonly_aug_ordered.csv')
 
 count = 1
 prev_seqid = 1
 processed_df = df
 processed_df = processed_df.iloc[0:0]
+
+df.sort_values(['sequenceID', 'eventID'], ascending=[True, True], inplace=True)
 
 for idx, row in df.iterrows():
     # print('.')
@@ -19,4 +21,5 @@ for idx, row in df.iterrows():
     processed_df = processed_df.append(row)
     count = count + 1
 
-processed_df.to_csv('hostonly_july_ordered.csv')
+processed_df.to_csv('hostonly_aug_ordered.csv')
+processed_df.to_csv(r'hostonly_aug_ordered.txt', header=None, index=None, sep=' ', mode='a')
